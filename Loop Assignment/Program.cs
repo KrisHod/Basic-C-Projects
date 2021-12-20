d4b32891b31a864ce17e9b4b01d36a81e7a3d878
Kris <krisgodom@gmail.com>
2021-12-20 15:01:01 -0400

    add Loop Assignment

---
+++ Loop Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Loop_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+            while (a < 10)
+            {
+                Console.WriteLine("\"a\"= {0}", a);
+                a++;
+            }
+            int b = -1;
+
+            do{
+                Console.WriteLine("\"b\"= {0}", b);
+                 b--;
+            }
+            while (b > -10);
+            Console.ReadLine();
+        }
+    }
+}
