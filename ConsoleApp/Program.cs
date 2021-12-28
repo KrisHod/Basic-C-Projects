8751bce83c8ff0f13b57267f525cae6593ef026c
Kris <krisgodom@gmail.com>
2021-12-28 13:40:46 -0400

    debug

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Boris", "Katia", "Ivan", "Eva", "Afonia", "Boris"};
+            List<string> newList = new List<string>();
+
+            foreach(string name in names){
+   
+                if (newList.Contains(name))
+                {
+                    Console.WriteLine(name + " has already appeared in the list");
+                }
+                else{
+                    Console.WriteLine(name);
+                }
+
+            newList.Add(name);
+            }
+            Console.Read();
+          
+        }
+    }
+}
