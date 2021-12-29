3316fb0f7b221e7ca52f266471c4bcff3fd26a76
Kris <krisgodom@gmail.com>
2021-12-29 11:10:58 -0400

    debug Exception assignment

--- Exception Assignment/Program.cs
+++ Exception Assignment/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Exception_Assignment
 {
@@ -11,23 +9,25 @@ namespace Exception_Assignment
         static void Main(string[] args)
         {
         List<int> list = new List<int>(){ 4, 7, 23, 56, 62, 4, 1, 67, 9};
-
-            Console.WriteLine("Enter a number to divide each number in the list by");
+            try
+            {
+                Console.WriteLine("Enter a number to divide each number in the list by");
             int input = Convert.ToInt32(Console.ReadLine());
-            try{
+            
                 foreach (int i in list)
                 {
                     Console.WriteLine(i / input);
                 }
             }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("You cannot divide by 0");
-            }
             catch (FormatException ex)
             {
                 Console.WriteLine("Please enter the whole number");
             }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divide by 0");
+            }
+            
             catch (Exception ex){
                 Console.WriteLine(ex.Message);
             }
