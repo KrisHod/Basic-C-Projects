e6c239590baa5c770ba0a5cf34b182c695746f36
Kris <krisgodom@gmail.com>
2021-12-31 17:04:21 -0400

    add method assignment

---
+++ Method Assignment/MathOper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    internal class MathOper
+    {
+
+        public int doubleNumber (int num){
+            return num * 2;
+        }
+
+        public int divideByTwo(int num)
+        {
+            return num / 2;
+        }
+
+        public int increaseByTwo(int num)
+        {
+            return num + 2;
+        }
+    }
+}
---
+++ Method Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            MathOper mathOper = new MathOper();
+            Console.WriteLine(mathOper.doubleNumber(a));
+            Console.WriteLine(mathOper.divideByTwo(a));
+            Console.WriteLine(mathOper.increaseByTwo(a));
+            Console.Read();
+        }
+    }
+}
