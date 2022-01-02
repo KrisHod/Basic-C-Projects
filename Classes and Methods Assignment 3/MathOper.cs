4172c83a7247bc14fdae7e4cd8deadf050770cff
Kris <krisgodom@gmail.com>
2022-01-02 14:01:15 -0400

    add classes and methods assignment 3

---
+++ Classes and Methods Assignment 3/MathOper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment_3
+{
+    internal class MathOper
+    {
+        public void add (int num1, int num2){
+        int res = num1 + num2;
+        Console.WriteLine(res);
+        }
+    }
+}
---
+++ Classes and Methods Assignment 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MathOper mathOper = new MathOper();
+            mathOper.add(3, 4);
+            mathOper.add(num1: 5, num2: 6);
+            Console.Read();
+        }
+    }
+}
