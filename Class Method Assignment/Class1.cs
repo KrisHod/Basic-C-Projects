5d14ab7bf5a07c79ed3c0f2ac3e25874f46d4b4d
Kris <krisgodom@gmail.com>
2022-01-02 16:44:18 -0400

    add class method assignment

---
+++ Class Method Assignment/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    internal static class Class1
+    {
+        public static void DivideBy2(int a)
+        {
+            Console.WriteLine("Your number divided by 2 is " + a/2);
+        }
+        public static void DivideBy2 (int a, out int output){
+        output = a/2;
+        }
+    }
+}
---
+++ Class Method Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class1 class1 = new Class1();
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //class1.DivideBy2(num);
+            Class1.DivideBy2(num);
+
+            int output = 0;
+            //class1.DivideBy2(num, out output);
+            Class1.DivideBy2(num, out output);
+            Console.WriteLine("output parameter equals " + output);
+            Console.ReadLine();
+
+        }
+    }
+}
