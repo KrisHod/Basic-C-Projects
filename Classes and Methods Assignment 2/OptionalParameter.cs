8ecd1af88a334140ce4c95dcaeeae9fcb668f046
Kris <krisgodom@gmail.com>
2022-01-02 13:50:55 -0400

    add classes and methods assignment 2

---
+++ Classes and Methods Assignment 2/OptionalParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    internal class OptionalParameter
+    {
+        public int subtract (int a, int b = 1){
+        return a - b;
+        }
+    }
+}
---
+++ Classes and Methods Assignment 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            OptionalParameter optionalParameter = new OptionalParameter();
+            
+
+            Console.WriteLine("Enter one or two whole numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            if (input == "")
+            {
+                Console.WriteLine(optionalParameter.subtract(a));
+            }
+            else
+            {
+                int b = Convert.ToInt32(input);
+                Console.WriteLine(optionalParameter.subtract(a, b));
+            }
+            Console.ReadLine();
+        }
+    }
+}
