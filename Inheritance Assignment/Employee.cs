b89962f30966309545e77b816b1b05a90b28a4e7
Kris <krisgodom@gmail.com>
2022-01-04 14:47:35 -0400

    refactor

--- Inheritance Assignment/Employee.cs
+++ Inheritance Assignment/Employee.cs
@@ -13,5 +13,9 @@ namespace Inheritance_Assignment
         public static bool operator== (Employee emp1, Employee emp2){
         return emp1.Id==emp2.Id;
         }
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            return emp1.Id != emp2.Id;
+        }
     }
 }
