dcbfb6f2addd04c941199618d16ffdfedfb9001c
Kris <krisgodom@gmail.com>
2022-01-03 21:15:18 -0400

    add interfaces assignment

---
+++ Interfaces Assignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Assignment
+{
+    internal class Employee: IQuittable
+    {
+        public int Id { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("I quit!!!");
+        }
+    }
+}
