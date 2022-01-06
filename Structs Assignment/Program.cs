1003771a8a1612a97215bbe76d6427d6873325d4
Kris <krisgodom@gmail.com>
2022-01-05 20:54:48 -0400

    add structs Assignment

---
+++ Structs Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Structs_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Number number = new Number();
+            number.Amount = 1000m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
