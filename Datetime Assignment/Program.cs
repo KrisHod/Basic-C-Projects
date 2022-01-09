2e2d4f0822a75283dd8caf4e66b891eb9ef5e3cb
Kris <krisgodom@gmail.com>
2022-01-09 13:41:48 -0400

    add Datetime assignment

---
+++ Datetime Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter the number, please");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In " + hours + " hours will be " + DateTime.Now.AddHours(hours));
+            Console.Read();
+        }
+    }
+}
