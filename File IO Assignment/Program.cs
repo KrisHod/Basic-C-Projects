a8eefaf74e77bbcb21d0a73fa8d1a11f56685918
Kris <krisgodom@gmail.com>
2022-01-09 12:38:51 -0400

    add File IO assignment

---
+++ File IO Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace File_IO_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number, please");
+            File.WriteAllText(@"C:\Users\Zver\Desktop\Basic_C#_Programs\Basic-C-Sharp-Projects\File IO Assignment\log.txt", Console.ReadLine());
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Zver\Desktop\Basic_C#_Programs\Basic-C-Sharp-Projects\File IO Assignment\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
