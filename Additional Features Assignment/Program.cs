43d775991ba2683a951fbdfeb2f33a18a64d5d86
Kris <krisgodom@gmail.com>
2022-01-11 20:51:15 -0400

    add Additional Features Assignment

---
+++ Additional Features Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string courseName = "C# and .NET Framework";
+
+            var student1 = new Student("Sasha", 78);
+            var student2 = new Student("Kostya");
+
+            Console.WriteLine(student1.Name + " and " + student2.Name + " are students of " + courseName + " course");
+            Console.Read();
+
+        }
+    }
+}
---
+++ Additional Features Assignment/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    public class Student
+    {
+        public Student(string name, int avgGrade)
+        {
+            Name = name;
+            AvgGrade = avgGrade;
+        }
+
+        public Student(string name) : this(name, 0)
+        {
+
+        }
+        public string Name { get; set; }
+        public int AvgGrade { get; set; }
+    }
+}
